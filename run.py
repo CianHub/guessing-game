7ab1c98f83193464fa8f36227f914e061a84247c
Cian O'Gralaigh <ihatecian@gmail.com>
2018-04-02 12:26:17 +0000

    added a function to check if data is contained in a specified file, added functionality to make usernames unique if an existing username is entered the username will not be written again and user will be logged in

--- run.py
+++ run.py
@@ -4,47 +4,30 @@ from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
-
 def write_to_file(filename, data):
-    """Handle the process of writing data to txt file"""
+    #Writes data to a specified file#
     with open(filename,"a") as file:
         file.writelines(data)
-    
 
+def check_in_file(filename, data):
+    #Checks if data is in a specified file#
+    with open(filename) as file:
+        content = file.read()
+        if data in content:
+            return True
+        else:
+            return False
+        
 @app.route('/', methods=["GET", "POST"])
+
 def index():
-    #If a user submits via the submit
+    #If a user submits via the submit username button, a unique username is stored and user is redirected to their page, if username already exists user is logged in #
     if request.method == "POST":
-        write_to_file("data/users.txt", request.form["username"] + "\n")
-        return redirect(request.form["username"])
+        if check_in_file("data/users.txt", request.form["username"].title()):
+            return redirect(request.form["username"])
+        else:
+            write_to_file("data/users.txt", request.form["username"].title() + "\n")
+            return redirect(request.form["username"])
     return render_template("index.html")
     
-def add_answers(username, answer):
-    """Add answers to the `answer text file"""
-    write_to_file("data/incorrect_answers.txt", "({0}) {1} - {2}\n".format(
-        datetime.now().strftime('%H:%M:%S'),
-        username.title(),
-        answer))
-
-def print_incorrect_answers():
-    answers = []
-    with open("data/incorrect_answers.txt", "r") as incorrect_answers:
-        answers = incorrect_answers.readlines()
-    return answers
-
-@app.route('/<username>', methods=["GET", "POST"])
-def user(username):
-    """Display riddle answers"""
-    answers = print_incorrect_answers()
-    if request.method == "POST":
-        return redirect('/' + username + '/' + request.form["answer"])
-    return render_template("riddle.html", username=username, incorrect_answers=answers)
-
-@app.route('/<username>/<answer>')
-def send_answer(username, answer):
-    """ Create a new answer and redirect back to the riddle page"""
-    add_answers(username, answer)
-    return redirect(username)
-
-
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
