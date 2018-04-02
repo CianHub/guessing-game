9dd34c6525a1087cd9c6df72e4bea6fcd500705d
Cian O'Gralaigh <ihatecian@gmail.com>
2018-04-02 15:49:34 +0000

    Added print content function, added store incorrect answers function which writes incorrect answers to the file, added check answers function which checks if the answer is incorrect, added functionality where user can submit answers and have incorrect answers display under the submission box

--- run.py
+++ run.py
@@ -1,11 +1,67 @@
 import os
-from flask import Flask
+from datetime import datetime
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+def write_to_file(filename, data):
+    #Writes data to a specified file#
+    with open(filename,"a") as file:
+        file.writelines(data)
+
+def check_in_file(filename, data):
+    #Checks if data is in a specified file#
+    with open(filename) as file:
+        content = file.read()
+        if data in content:
+            return True
+        else:
+            return False
+            
+def print_content(filename):
+    # Prints content from a specified file #
+    content = []
+    with open(filename, "r") as file_content:
+        content = file_content.readlines()
+    return content
+        
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return "<h1> Hello World </h1>"
+    #If a user submits via the submit username button, a unique username is stored and user is redirected to their page, if username already exists user is logged in #
+    if request.method == "POST":
+        if check_in_file("data/users.txt", request.form["username"].title()):
+            return redirect(request.form["username"])
+        else:
+            write_to_file("data/users.txt", request.form["username"].title() + "\n")
+            return redirect(request.form["username"])
+    return render_template("index.html")
+ 
+def store_incorrect_answers(username, answer):
+    # write incorrect answers to the incorrect answer text file #
+    write_to_file("data/incorrect_answers.txt", "({0}) {1} - {2}\n".format(
+        datetime.now().strftime('%H:%M:%S'),
+        username.title(),
+        answer))
 
+def check_answers(username, answer):
+    # Check if answer is incorrect and if so add the incorrect answer to the incorrect answer text file #
+    if check_in_file("data/riddles.json", answer):
+        print("correct")
+    else:
+        print("incorrect")
+        store_incorrect_answers(username, answer)
+        
+@app.route('/<username>', methods=["GET", "POST"])
+def user(username):
+    answers = print_content("data/incorrect_answers.txt")
+    if request.method == "POST":
+        return redirect('/' + username + '/' + request.form["answer"])
+    return render_template("riddle.html", username=username, incorrect_answers=answers)
+
+@app.route('/<username>/<answer>')
+def send_answer(username, answer):
+    """ Create a new answer and redirect back to the riddle page"""
+    check_answers(username, answer )
+    return redirect(username)
+  
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
-
