949cadd2b8a657644743749137740185bd9d27bb
Cian O'Gralaigh <ihatecian@gmail.com>
2018-04-02 21:36:01 +0000

    Test commit

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 import os
+import json
 from datetime import datetime
 from flask import Flask, redirect, render_template, request
+from socket import *
+
 
 app = Flask(__name__)
 
@@ -17,6 +20,21 @@ def check_in_file(filename, data):
             return True
         else:
             return False
+
+def check_riddle(answer, qnumber):
+    #Checks if the answer to the riddle is correct#
+    with open("data/riddles.json", "r") as riddles:
+        
+        content = json.load(riddles)
+        for check in content:
+            print qnumber
+            if check["answer"] in answer and check["number"] in qnumber:
+                print check["answer"]
+                return True
+            elif check["answer"] not in answer:
+                print check["answer"]
+                continue
+                return False 
             
 def print_content(filename):
     # Prints content from a specified file #
@@ -27,13 +45,13 @@ def print_content(filename):
         
 @app.route('/', methods=["GET", "POST"])
 def index():
-    #If a user submits via the submit username button, a unique username is stored and user is redirected to their page, if username already exists user is logged in #
+    # If a user submits via the submit username button, a unique username is stored and user is redirected to their page, if username already exists user is logged in #
     if request.method == "POST":
         if check_in_file("data/users.txt", request.form["username"].title()):
-            return redirect(request.form["username"])
+            return redirect(request.form["username"] + '/' + '0')
         else:
             write_to_file("data/users.txt", request.form["username"].title() + "\n")
-            return redirect(request.form["username"])
+            return redirect(request.form["username"] + '/' + '0')
     return render_template("index.html")
  
 def store_incorrect_answers(username, answer):
@@ -43,25 +61,40 @@ def store_incorrect_answers(username, answer):
         username.title(),
         answer))
 
-def check_answers(username, answer):
+def check_answers(username, answer, qnumber):
     # Check if answer is incorrect and if so add the incorrect answer to the incorrect answer text file #
-    if check_in_file("data/riddles.json", answer):
+    if check_riddle(answer, qnumber):
         print("correct")
+        return True
     else:
         print("incorrect")
         store_incorrect_answers(username, answer)
         
-@app.route('/<username>', methods=["GET", "POST"])
-def user(username):
+@app.route('/<username>/<qnumber>', methods=["GET", "POST"])
+def user(username, qnumber):
+    riddle = {}
     answers = print_content("data/incorrect_answers.txt")
-    if request.method == "POST":
-        return redirect('/' + username + '/' + request.form["answer"])
-    return render_template("riddle.html", username=username, incorrect_answers=answers)
+    
+    with open("data/riddles.json", "r") as content:
+        data = json.load(content)
+        for question in data:
+            if question["number"] in qnumber:
+                riddle = question["question"]
+        
+        if request.method == "POST":
+            return redirect('/' + username + '/' + qnumber + '/' + request.form["answer"])
+            
+    return render_template("riddle.html", username=username, incorrect_answers=answers, riddle=riddle)
 
-@app.route('/<username>/<answer>')
-def send_answer(username, answer):
+@app.route('/<username>/<qnumber>/<answer>')
+def send_answer(username, qnumber, answer):
     """ Create a new answer and redirect back to the riddle page"""
-    check_answers(username, answer )
-    return redirect(username)
-  
+    if check_answers(username, answer, qnumber):
+        new_q = str(int(qnumber) + 1)
+        qnumber = new_q
+    if qnumber > 2:
+        return render_template("leaderboard.html")
+        
+    return redirect('/' + username + '/' + qnumber)
+ 
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
