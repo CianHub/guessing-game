f5ac446e2ed1bc9065c814d85bd25b32b80b93a5
Cian O'Gralaigh <ihatecian@gmail.com>
2018-04-16 17:14:24 +0000

    Added multiple user functionality, made minor fixes to exisiting functions, added riddles, added rounds and turns, cleaned up and commented code, improved usability

--- functions.py
+++ functions.py
@@ -40,15 +40,15 @@ def print_content_in_list(filename):
  
 # ..................................Riddle Game Functions
 
-def check_answer(answer, qnumber):
+def check_answer(answer,rnumber, question):
     #Checks if the answer to the riddle is correct#
     with open("data/riddles.json", "r") as riddles:
-        content = json.load(riddles)
-        for check in content:
-            if check["answer"].title() in answer.title() and check["number"] in qnumber:
+        data = json.load(riddles)
+        for i in range(len(data['riddles'])):
+            if answer.title() == data['riddles'][i]["answer"] and question == data['riddles'][i]["number"]:
                 clear_text_file("data/incorrect_answers.txt")
                 return True
-            elif check["answer"] not in answer:
+            elif data['riddles'][i]["answer"]  not in answer:
                 continue
         
 def store_incorrect_answers(username, answer):
@@ -59,50 +59,102 @@ def store_incorrect_answers(username, answer):
         answer))
     return check_in_file("data/incorrect_answers.txt", answer)
 
-def right_or_wrong(username, answer, qnumber):
-    # Check if answer is correct #
-    if check_answer(answer, qnumber):
+def right_or_wrong(username, answer, qnumber, rnumber, question):
+    # Handles answer results #
+    if check_answer(answer, rnumber, question):
         increase_user_score(username)
         return True
     else:
         store_incorrect_answers(username, answer)
 
-def question_selector(qnumber):
+def next_player(username, qnumber):
+    # Moves the turn to the next player #
+    with open("data/leaderboard.json", "r") as json_file:
+        json_decoded = json.load(json_file)
+        for i in range(len(json_decoded['leaderboard'])):
+            if qnumber in json_decoded['leaderboard'][i]["qnumber"]:
+                username = json_decoded['leaderboard'][i]["username"]
+                return username
+  
+def question_selector(question, rnumber):
     # Selects the current question #
     riddle = {}
     with open("data/riddles.json", "r") as content:
         data = json.load(content)
-        for question in data:
-            if question["number"] == qnumber:
-                riddle = question["question"]
+        for i in range(len(data['riddles'])):
+            if question == data['riddles'][i]["number"] and rnumber == data['riddles'][i]["round"]:
+                riddle = data['riddles'][i]["question"]
+                print (data['riddles'][i]["question"])
                 return riddle
             
 def increase_user_score(username):
-    # Increases user score by 1 #
+    # Increments a user's score #
     with open("data/leaderboard.json", "r") as json_file:
         json_decoded = json.load(json_file)
         for i in range(len(json_decoded['leaderboard'])):
             if username in json_decoded['leaderboard'][i]["username"]:
                 json_decoded['leaderboard'][i]["score"] = json_decoded['leaderboard'][i]["score"] + 1
-                return json_decoded['leaderboard'][i]["score"]
         with open("data/leaderboard.json", 'w') as new_file:
             json.dump(json_decoded, new_file)
-        
+            return json_decoded['leaderboard'][i]["score"]
+
+def setup_leaderboard():
+    # Initialises the leaderboard #
+        data = {"leaderboard": []}
+        with open("data/leaderboard.json", 'w') as new_file:
+            json.dump(data, new_file)
+   
 def add_to_leaderboard(username):
     # Adds new users and a starting score to leaderboard #
     with open("data/leaderboard.json", "r") as json_file:
         json_decoded = json.load(json_file)
-        data = { "username": username, "score": 0}
+        data = { "username": username, "score": 0, "qnumber": str(len(json_decoded["leaderboard"]))}
         json_decoded['leaderboard'].append(data)
         with open("data/leaderboard.json", 'w') as new_file:
             json.dump(json_decoded, new_file)
             new_file.close()
             return check_in_file("data/leaderboard.json", data["username"])
 
-def fix_leaderboard():
-    # Rearranges leaderboard according to score #
+def order_leaderboard():
+    # Orders the leaderboard according to score #
     with open("data/leaderboard.json", "r") as json_file:
         json_decoded = json.load(json_file)
         sorted_leaderboard = sorted(json_decoded['leaderboard'], reverse=True, key=itemgetter("score"))
-        print(sorted_leaderboard)
         return sorted_leaderboard
+        
+def leaderboard_len(leaderboard):
+    # Gets the length of the leaderboard #
+    with open(leaderboard, "r") as json_file:
+        json_decoded = json.load(json_file)
+        return len(json_decoded['leaderboard'])
+        
+def next_round(rnumber):
+    # Increments the rnumber #
+    rnumber = str(int(rnumber) + 1)
+    return rnumber
+
+def reset_turn(qnumber):
+    # Starts the turn cycle again e.g. after everyone has a turn it is the first player's turn again #
+    qnumber = str(leaderboard_len("data/leaderboard.json") - 1)
+    return qnumber
+    
+def q_update(qnumber):
+    # Decrements the qnumber #
+    new_q = str(int(qnumber) - 1)
+    qnumber = new_q
+    return qnumber
+
+def question_update(question):
+    # Changes the current question number #
+    next_question = str(int(question) + 1)
+    question = next_question
+    return question
+
+def get_winner():
+    # Gets the winner from the ordered leaderboard #
+    leaderboard = order_leaderboard()
+    winner = leaderboard[0]["username"]
+    return winner
+
+
+        
--- run.py
+++ run.py
@@ -3,14 +3,14 @@
 import os
 import json
 from datetime import datetime
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, flash
 import io
 try:
     to_unicode = unicode
 except NameError:
     to_unicode = str
 from operator import itemgetter
-from functions import write_to_file, check_in_file, clear_text_file, print_content_in_list, check_answer, store_incorrect_answers, right_or_wrong, question_selector, increase_user_score, add_to_leaderboard, fix_leaderboard
+from functions import write_to_file, check_in_file, clear_text_file, print_content_in_list, check_answer, store_incorrect_answers, right_or_wrong, question_selector, increase_user_score, add_to_leaderboard, order_leaderboard, setup_leaderboard, next_player, leaderboard_len, next_round, reset_turn, q_update, get_winner, question_update
 
 app = Flask(__name__)
 
@@ -18,41 +18,77 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET", "POST"])
 def index():
-    # If a user submits via the submit username button, a unique username is stored and user is redirected to their page, if username already exists user is logged in #
+    # User(s) enter the number of players in the game #
+    setup_leaderboard()
+    clear_text_file("data/users.txt")
     if request.method == "POST":
-        if check_in_file("data/users.txt", request.form["username"].title()):
-            return redirect(request.form["username"] + '/' + '0')
-        else:
-            write_to_file("data/users.txt", request.form["username"].title() + "\n")
-            add_to_leaderboard(request.form["username"].title())
-            return redirect(request.form["username"] + '/' + '0')
+        #if request.form["playernum"] > 4:
+            # Code about error too many players #
+        #else:
+            # below code #
+        playernum = request.form["playernum"]
+        return redirect("setup" + '/' + playernum)
     return render_template("index.html")
  
-@app.route('/<username>/<qnumber>', methods=["GET", "POST"])
-def user(username, qnumber):
-    answers = print_content_in_list("data/incorrect_answers.txt")
-    riddle = question_selector(qnumber)
+@app.route('/setup/<playernum>', methods=["GET", "POST"])
+def username(playernum):
+    # User(s) enter their desired usernames #
+    instruct_num = str(leaderboard_len("data/leaderboard.json") + 1)
     if request.method == "POST":
-        return redirect('/' + username + '/' + qnumber + '/' + request.form["answer"])
+        new_num = str(int(playernum) - 1)
+        playernum = new_num
+        if not check_in_file("data/users.txt", request.form["username"].title()):
+            write_to_file("data/users.txt", request.form["username"].title() + "\n")
+            add_to_leaderboard(request.form["username"].title())
+        #else:
+            #code about error message for duplicate usernames #
         
-    return render_template("riddle.html", username=username, incorrect_answers=answers, riddle=riddle, qnumber=str(int(qnumber) + 1))
-
-@app.route('/<username>/<qnumber>/<answer>')
-def send_answer(username, qnumber, answer):
+        if int(playernum) == 0:
+                qnumber = str(leaderboard_len("data/leaderboard.json") - 1)
+                return redirect(request.form["username"] + '/' + '1' + '/' + qnumber + '/' + '0')
+        else:
+            return redirect("setup" + '/' + playernum)
+    return render_template("username.html", player=instruct_num)
+ 
+@app.route('/<username>/<rnumber>/<qnumber>/<question>', methods=["GET", "POST"])
+def riddle(username, rnumber, qnumber, question):
+    # Main game page, questions are displayed, can be answered and wrong answers are displayed #
+    incorrect_answers = print_content_in_list("data/incorrect_answers.txt")
+    riddle = question_selector(question, rnumber)
+    if request.method == "POST":
+        return redirect('/' + username + '/' + rnumber + '/' + qnumber + '/' + question  + '/' + request.form["answer"])
+    return render_template("riddle.html", username=username, incorrect_answers=incorrect_answers, riddle=riddle, rnumber= rnumber)
+    
+@app.route('/<username>/<rnumber>/<qnumber>/<question>/<answer>')
+def send_answer(username, qnumber, answer, rnumber, question):
     # Create a new answer and redirect back to the riddle page
-    if right_or_wrong(username, answer, qnumber):
-        new_q = str(int(qnumber) + 1)
-        qnumber = new_q
-    if int(qnumber) > 2:
-        return redirect('/' + username + "/leaderboard")
-    return redirect('/' + username + '/' + qnumber)
+    if right_or_wrong(username, answer, qnumber, rnumber, question):
+        qnumber = q_update(qnumber)
+        question = question_update(question)
+        if int(qnumber) == -1:
+            rnumber = next_round(rnumber)
+            qnumber = reset_turn(qnumber)
+            username = next_player(username, qnumber)
+        else:
+            username = next_player(username, qnumber)
+    else:
+        qnumber = q_update(qnumber)
+        if int(qnumber) == -1:
+            qnumber = reset_turn(qnumber)
+            username = next_player(username, qnumber)
+        else:
+            username = next_player(username, qnumber)
+    if int(rnumber) > 3:
+        return redirect("/leaderboard")
+    return redirect('/' + username + '/' + rnumber + '/' + qnumber + '/' + question)
 
-@app.route('/<username>/leaderboard', methods=["GET", "POST"])
-def leaderboard(username):
-    # Calls organised leaderboard and leaderboard.html page #
+@app.route('/leaderboard', methods=["GET", "POST"])
+def leaderboard():
+    # Displays leaderboard and winner #
     number = 0
-    leaderboard = fix_leaderboard()
-    return render_template("leaderboard.html", username=username, leaderboard=leaderboard, number=number)
+    winner = get_winner()
+    leaderboard = order_leaderboard()
+    return render_template("leaderboard.html", username=winner, leaderboard=leaderboard, number=number)
 
 # ..................................Run Web App
 
