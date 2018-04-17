0aa8fdfe79e50c5ef1230a69278258e8d4eb8cb3
Cian O'Gralaigh <ihatecian@gmail.com>
2018-04-17 22:27:54 +0000

    refactored automated test and functions and improved code readability

--- run.py
+++ run.py
@@ -3,56 +3,100 @@
 import os
 import json
 from datetime import datetime
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, flash
 import io
 try:
     to_unicode = unicode
 except NameError:
     to_unicode = str
 from operator import itemgetter
-from functions import write_to_file, check_in_file, clear_text_file, print_content_in_list, check_answer, store_incorrect_answers, right_or_wrong, question_selector, increase_user_score, add_to_leaderboard, fix_leaderboard
+from functions import write_to_file, check_in_file, clear_text_file, print_content_in_list, check_answer, store_incorrect_answers, right_or_wrong, question_selector, increase_user_score, add_to_leaderboard, order_leaderboard, setup_leaderboard, next_player, leaderboard_len, next_round, reset_turn, q_update, get_player_name, question_update, random_number_generator_dependent, init_riddles, get_player_score, random_number_generator, init_game
 
 app = Flask(__name__)
+app.secret_key = 'some_secret'
 
 # ..................................Template Page Functions
 
 @app.route('/', methods=["GET", "POST"])
 def index():
-    # If a user submits via the submit username button, a unique username is stored and user is redirected to their page, if username already exists user is logged in #
+    # User(s) enter the number of players in the game #
+    init_game()
     if request.method == "POST":
-        if check_in_file("data/users.txt", request.form["username"].title()):
-            return redirect(request.form["username"] + '/' + '0')
+        if int(request.form["playernum"]) > 4 or int(request.form["playernum"])  < 1:
+            flash("Please Enter a Valid Number of Players (Max. 4, Min. 1)")
+            return redirect("/")
         else:
-            write_to_file("data/users.txt", request.form["username"].title() + "\n")
-            add_to_leaderboard(request.form["username"].title())
-            return redirect(request.form["username"] + '/' + '0')
+            playernum = request.form["playernum"]
+            return redirect("setup" + '/' + playernum)
     return render_template("index.html")
  
-@app.route('/<username>/<qnumber>', methods=["GET", "POST"])
-def user(username, qnumber):
-    answers = print_content_in_list("data/incorrect_answers.txt")
-    riddle = question_selector(qnumber)
+@app.route('/setup/<playernum>', methods=["GET", "POST"])
+def username(playernum):
+    # User(s) enter their desired usernames #
+    instruct_num = str(leaderboard_len("data/leaderboard.json") + 1)
     if request.method == "POST":
-        return redirect('/' + username + '/' + qnumber + '/' + request.form["answer"])
+        if not check_in_file("data/users.txt", request.form["username"].title()):
+            write_to_file("data/users.txt", request.form["username"].title() + "\n")
+            add_to_leaderboard(request.form["username"].title())
+            new_num = str(int(playernum) - 1)
+            playernum = new_num
+        else:
+            flash("Sorry Your Chosen Username Is Unavailable. Please Try Another")
+            return redirect("/setup" + '/' + playernum)
         
-    return render_template("riddle.html", username=username, incorrect_answers=answers, riddle=riddle, qnumber=str(int(qnumber) + 1))
-
-@app.route('/<username>/<qnumber>/<answer>')
-def send_answer(username, qnumber, answer):
+        if int(playernum) == 0:
+                qnumber = str(leaderboard_len("data/leaderboard.json") - 1)
+                question = random_number_generator()
+                return redirect(request.form["username"] + '/' + '1' + '/' + qnumber + '/' + question)
+        else:
+            return redirect("setup" + '/' + playernum)
+    return render_template("username.html", player=instruct_num)
+ 
+@app.route('/<username>/<rnumber>/<qnumber>/<question>', methods=["GET", "POST"])
+def riddle(username, rnumber, qnumber, question):
+    # Main game page, questions are displayed, can be answered and wrong answers are displayed #
+    incorrect_answers = print_content_in_list("data/incorrect_answers.txt")
+    riddle = question_selector(question)
+    if request.method == "POST":
+        return redirect('/' + username + '/' + rnumber + '/' + qnumber + '/' + question  + '/' + request.form["answer"])
+    return render_template("riddle.html", username=username, incorrect_answers=incorrect_answers, riddle=riddle, rnumber= rnumber)
+    
+@app.route('/<username>/<rnumber>/<qnumber>/<question>/<answer>')
+def send_answer(username, qnumber, answer, rnumber, question):
     # Create a new answer and redirect back to the riddle page
-    if right_or_wrong(username, answer, qnumber):
-        new_q = str(int(qnumber) + 1)
-        qnumber = new_q
-    if int(qnumber) > 2:
-        return redirect('/' + username + "/leaderboard")
-    return redirect('/' + username + '/' + qnumber)
+    if right_or_wrong(username, answer, question):
+        qnumber = q_update(qnumber)
+        question = question_update(question)
+        if int(qnumber) == -1:
+            rnumber = next_round(rnumber)
+            qnumber = reset_turn(qnumber)
+            username = next_player(username, qnumber)
+        else:
+            username = next_player(username, qnumber)
+    else:
+        qnumber = q_update(qnumber)
+        if int(qnumber) == -1:
+            qnumber = reset_turn(qnumber)
+            username = next_player(username, qnumber)
+        else:
+            username = next_player(username, qnumber)
+    if int(rnumber) > 3:
+        return redirect("/leaderboard")
+    return redirect('/' + username + '/' + rnumber + '/' + qnumber + '/' + question)
 
-@app.route('/<username>/leaderboard', methods=["GET", "POST"])
-def leaderboard(username):
-    # Calls organised leaderboard and leaderboard.html page #
+@app.route('/leaderboard', methods=["GET", "POST"])
+def leaderboard():
+    # Displays leaderboard and winner #
     number = 0
-    leaderboard = fix_leaderboard()
-    return render_template("leaderboard.html", username=username, leaderboard=leaderboard, number=number)
+    leaderboard = order_leaderboard()
+    winner = get_player_name(0)
+    winner_score = get_player_score(0)
+    runner_up = "blank"
+    runner_up_score = 0
+    if leaderboard_len("data/leaderboard.json") > 1:
+        runner_up_score = get_player_score(1)
+        runner_up = get_player_name(1)
+    return render_template("leaderboard.html", winner=winner, winner_score=winner_score, runner_up_score=runner_up_score, runner_up=runner_up, leaderboard=leaderboard, number=number)
 
 # ..................................Run Web App
 
--- test_functions.py
+++ test_functions.py
@@ -1,13 +1,27 @@
-from functions import write_to_file, check_in_file, clear_text_file, print_content_in_list, check_answer, store_incorrect_answers, right_or_wrong, question_selector, increase_user_score, add_to_leaderboard, fix_leaderboard
-import unittest 
-import json
 import os
 import json
-from flask import Flask, redirect, render_template, request
+from datetime import datetime
+import io
+try:
+    to_unicode = unicode
+except NameError:
+    to_unicode = str
+from operator import itemgetter
+from functions import write_to_file, check_in_file, clear_text_file, print_content_in_list, check_answer, store_incorrect_answers, right_or_wrong, question_selector, increase_user_score, add_to_leaderboard, order_leaderboard, setup_leaderboard, next_player, leaderboard_len, next_round, reset_turn, q_update, get_player_name, question_update, random_number_generator, init_riddles, get_player_score, random_number_generator_dependent, init_game, riddle_len
+import unittest 
+
 
 class test_game(unittest.TestCase):
     
     # Test Suite #
+      
+    # ..................................Init Functions
+    
+    def test_init_riddles(self):
+        # Test if function returns True upon searching for a question in the file #
+        self.assertEqual(init_riddles('data/riddles.json'), True)
+    
+    # ..................................General Functions
     
     def test_clear_text_file(self):
         # Test if function clears a txt file #
@@ -27,9 +41,103 @@ class test_game(unittest.TestCase):
         # Test if function writes to a txt file #
         assert type(print_content_in_list("data/users.txt")) is list
     
+    def test_random_number_generator_dependent(self):
+        # Test if function returns a random number in str format #
+        with open("data/riddles.json", "r") as json_file:
+            json_decoded = json.load(json_file)
+            assert type(random_number_generator_dependent(json_decoded)) is str
+            assert type(int(random_number_generator_dependent(json_decoded))) is int
+    
+    def test_random_number_generator(self):
+        # Test if function returns a random number in str format #
+        assert type(random_number_generator()) is str
+        assert type(int(random_number_generator())) is int
+    
+    # ..................................Information Fetching Functions
+
+    def test_get_player_name(self):
+        # Test if function gets a players name #
+        setup_leaderboard()
+        add_to_leaderboard("Jason")
+        add_to_leaderboard("Zebra")
+        self.assertEqual(get_player_name(0), "Jason")
+        self.assertEqual(get_player_name(1), "Zebra")
+    
+    def test_get_player_score(self):
+        # Test if function gets a players name #
+        init_game()
+        add_to_leaderboard("Jason")
+        add_to_leaderboard("Zebra")
+        increase_user_score("Zebra")
+        self.assertEqual(get_player_score(1), 0)
+        self.assertEqual(get_player_score(0), 1)
+    
+    def test_leaderboard_len(self):
+        # Test if function returns length of the leaderboard in int format #
+        assert type(leaderboard_len('data/leaderboard.json')) is int
+    
+    def test_riddle_len(self):
+        # Test if function returns numbers of riddles in int format #
+        assert type(riddle_len('data/riddles.json')) is int
+    
+    # ..................................Round, Turn and Game Flow Functions
+    
+    def test_next_round(self):
+        # Test if function returns the rnumber incremented by 1 in str format #
+        assert type(next_round('0')) is str
+        self.assertEqual((int(next_round(0))), 1)
+    
+    def test_reset_turn(self):
+        # Test if function returns the leaderboard_len() decremnted by 1 in str format #
+        assert type(reset_turn('3')) is str
+        self.assertEqual((int(reset_turn('3'))), 2)
+    
+    def test_q_update(self):
+        # Test if function returns the qnumber decremnted by 1 in str format #
+        assert type(q_update('3')) is str
+        self.assertEqual((int(q_update('3'))), 2)
+    
+    def test_question_update(self):
+        # Test if function returns a random number in str format #
+        init_riddles('data/riddles.json')
+        assert type(question_update("1")) is str
+        assert type(int(question_update("1"))) is int
+    
+    def test_question_selector(self):
+        # Test if function selects the correct question #
+        self.assertEqual(question_selector('25'), None)
+        self.assertEqual(question_selector('0'), "I have a ring, but no hands. I used to be plugged into the wall but now I follow you every where. What am I?")
+    
+    # ..................................Leaderboard Functions
+    
+    def test_add_to_leaderboard(self):
+        # Test if function adds a new user and their score to leaderboard #
+        setup_leaderboard()
+        self.assertEqual(add_to_leaderboard('Jon'), True)
+        self.assertEqual(add_to_leaderboard('Paul'), True)
+        self.assertEqual(add_to_leaderboard('Sarah'), True)
+        
+    def test_order_leaderboard(self):
+        # Test if function returns a rearranged version of the leaderboard list #
+        with open("data/leaderboard.json", "r") as json_file:
+            loaded_json = json.load(json_file)
+            original_leaderboard = loaded_json['leaderboard']
+            assert type(order_leaderboard()) is list
+            self.assertNotEqual(order_leaderboard(), original_leaderboard)
+        
+    def test_increase_user_score(self):
+        # Test if function increases the users score on the leaderboard #
+        # add_to_leaderboard initialises a new users score at 0, so new user Petey will have a score of 0 to start with #
+        add_to_leaderboard("Petey")
+        self.assertEqual(increase_user_score("Petey"), 1)
+        self.assertGreater(increase_user_score('Petey'), 0)
+        self.assertNotEqual(increase_user_score('Petey'), 0)
+    
+    # ..................................Answer Checking Functions    
+    
     def test_check_answer(self):
         # Test if function checks if an answer is correct #
-        self.assertEqual(check_answer("The first answer", '0'), True)
+        self.assertEqual(check_answer("Telephone", '0'), True)
         self.assertEqual(check_answer("The wrong answer", '1'), None)
     
     def test_store_incorrect_answers(self):
@@ -38,37 +146,11 @@ class test_game(unittest.TestCase):
     
     def test_right_or_wrong(self):
         # Test if function checks if an answer is right or wrong #
-        self.assertEqual(right_or_wrong("Jason","The first answer", '0'), True)
+        init_riddles('data/riddles.json')
+        setup_leaderboard()
+        add_to_leaderboard("Jason")
+        add_to_leaderboard("Paul")
+        add_to_leaderboard("Eva")
+        self.assertEqual(right_or_wrong("Jason","Telephone", '0'), True)
         self.assertEqual(right_or_wrong("Paul","The wrong answer", '2'), None)
-        self.assertEqual(right_or_wrong("Eva","The second answer", '1'), True)
-    
-    def test_question_selector(self):
-        # Test if function selects the correct question #
-        assert type(question_selector("0")) is str
-        self.assertEqual(question_selector('15'), None)
-        assert type(question_selector("2")) is str
-        self.assertEqual(question_selector('F'), None)
-    
-    def test_add_to_leaderboard(self):
-        # Test if function adds a new user and their score to leaderboard #
-        self.assertEqual(add_to_leaderboard('Jon'), True)
-        self.assertEqual(add_to_leaderboard('Paul'), True)
-        self.assertEqual(add_to_leaderboard('Sarah'), True)
-    
-    def test_increase_user_score(self):
-        # Test if function increases the users score on the leaderboard #
-        # add_to_leaderboard initialises a new users score at 0, so new user Petey will have a score of 0 to start with #
-        add_to_leaderboard("Petey")
-        self.assertGreater(increase_user_score('Petey'), 0)
-        self.assertNotEqual(increase_user_score('Petey'), 0)
-    
-    def test_fix_leaderboard(self):
-        # Test if function returns a rearranged version of the leaderboard list #
-        with open("data/leaderboard.json", "r") as json_file:
-            loaded_json = json.load(json_file)
-            original_leaderboard = loaded_json['leaderboard']
-            assert type(fix_leaderboard()) is list
-            self.assertNotEqual(fix_leaderboard(), original_leaderboard)
- 
-
-       
+        self.assertEqual(right_or_wrong("Eva","Fall", '1'), True)
