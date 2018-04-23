85f64855c98e6b0879a0bb8f5ea5fb9568805b08
Cian O'Gralaigh <ihatecian@gmail.com>
2018-04-23 15:37:27 +0000

    added changes from testing branch

--- run.py
+++ run.py
@@ -5,7 +5,6 @@ import json
 from datetime import datetime
 from flask import Flask, redirect, render_template, request, flash
 import io
-<<<<<<< HEAD
 from operator import itemgetter
 from functions import(
     write_to_file, check_in_file, clear_text_file,
@@ -17,14 +16,6 @@ from functions import(
     init_riddles, get_player_score, random_number_generator, init_game,
     score_display, decrement_score, get_question_points
     )
-=======
-try:
-    to_unicode = unicode
-except NameError:
-    to_unicode = str
-from operator import itemgetter
-from functions import write_to_file, check_in_file, clear_text_file, print_content_in_list, check_answer, store_incorrect_answers, right_or_wrong, question_selector, increase_user_score, add_to_leaderboard, order_leaderboard, setup_leaderboard, next_player, leaderboard_len, next_round, reset_turn, q_update, get_player_name, question_update, random_number_generator_dependent, init_riddles, get_player_score, random_number_generator, init_game
->>>>>>> dc008c2f17a4d9e19c4b4a3d74b8e6f5673ca665
 
 app = Flask(__name__)
 app.secret_key = 'some_secret'
@@ -35,7 +26,6 @@ app.secret_key = 'some_secret'
 def index():
     # User(s) enter the number of players in the game #
     init_game()
-<<<<<<< HEAD
 
     if request.method == "POST":
         if int(
@@ -43,10 +33,6 @@ def index():
             ) > 4 or int(
                 request.form["playernum"]
                 ) < 1:
-=======
-    if request.method == "POST":
-        if int(request.form["playernum"]) > 4 or int(request.form["playernum"])  < 1:
->>>>>>> dc008c2f17a4d9e19c4b4a3d74b8e6f5673ca665
             flash("Please Enter a Valid Number of Players (Max. 4, Min. 1)")
             return redirect("/")
         else:
@@ -59,7 +45,6 @@ def username(playernum):
     # User(s) enter their desired usernames #
     instruct_num = str(leaderboard_len("data/leaderboard.json") + 1)
     if request.method == "POST":
-<<<<<<< HEAD
         if not (
             check_in_file(
                 "data/users.txt", request.form["username"].title())
@@ -67,15 +52,10 @@ def username(playernum):
             write_to_file(
                 "data/users.txt", request.form["username"].title() + "\n"
                 )
-=======
-        if not check_in_file("data/users.txt", request.form["username"].title()):
-            write_to_file("data/users.txt", request.form["username"].title() + "\n")
->>>>>>> dc008c2f17a4d9e19c4b4a3d74b8e6f5673ca665
             add_to_leaderboard(request.form["username"].title())
             new_num = str(int(playernum) - 1)
             playernum = new_num
         else:
-<<<<<<< HEAD
             flash(
                 "Sorry Your Chosen Username Is Unavailable. Please Try Another"
                 )
@@ -154,48 +134,10 @@ def send_answer(
         
         if int(qnumber) == -1:
             rnumber = next_round(rnumber)
-=======
-            flash("Sorry Your Chosen Username Is Unavailable. Please Try Another")
-            return redirect("/setup" + '/' + playernum)
-        
-        if int(playernum) == 0:
-                qnumber = str(leaderboard_len("data/leaderboard.json") - 1)
-                question = random_number_generator()
-                return redirect(request.form["username"] + '/' + '1' + '/' + qnumber + '/' + question)
-        else:
-            return redirect("setup" + '/' + playernum)
-    return render_template("username.html", player=instruct_num)
- 
-@app.route('/<username>/<rnumber>/<qnumber>/<question>', methods=["GET", "POST"])
-def riddle(username, rnumber, qnumber, question):
-    # Main game page, questions are displayed, can be answered and wrong answers are displayed #
-    incorrect_answers = print_content_in_list("data/incorrect_answers.txt")
-    riddle = question_selector(question)
-    if request.method == "POST":
-        return redirect('/' + username + '/' + rnumber + '/' + qnumber + '/' + question  + '/' + request.form["answer"])
-    return render_template("riddle.html", username=username, incorrect_answers=incorrect_answers, riddle=riddle, rnumber= rnumber)
-    
-@app.route('/<username>/<rnumber>/<qnumber>/<question>/<answer>')
-def send_answer(username, qnumber, answer, rnumber, question):
-    # Create a new answer and redirect back to the riddle page
-    if right_or_wrong(username, answer, question):
-        qnumber = q_update(qnumber)
-        question = question_update(question)
-        if int(qnumber) == -1:
-            rnumber = next_round(rnumber)
             qnumber = reset_turn(qnumber)
             username = next_player(username, qnumber)
         else:
             username = next_player(username, qnumber)
-    else:
-        qnumber = q_update(qnumber)
-        if int(qnumber) == -1:
->>>>>>> dc008c2f17a4d9e19c4b4a3d74b8e6f5673ca665
-            qnumber = reset_turn(qnumber)
-            username = next_player(username, qnumber)
-        else:
-            username = next_player(username, qnumber)
-<<<<<<< HEAD
             
     else:
         question_score = get_question_points(question)
@@ -222,11 +164,6 @@ def send_answer(username, qnumber, answer, rnumber, question):
     return redirect(
         '/' + username + '/' + rnumber + '/' + qnumber + '/' + question
         )
-=======
-    if int(rnumber) > 3:
-        return redirect("/leaderboard")
-    return redirect('/' + username + '/' + rnumber + '/' + qnumber + '/' + question)
->>>>>>> dc008c2f17a4d9e19c4b4a3d74b8e6f5673ca665
 
 @app.route('/leaderboard', methods=["GET", "POST"])
 def leaderboard():
@@ -240,15 +177,11 @@ def leaderboard():
     if leaderboard_len("data/leaderboard.json") > 1:
         runner_up_score = get_player_score(1)
         runner_up = get_player_name(1)
-<<<<<<< HEAD
     return render_template(
         "leaderboard.html", winner=winner,
         winner_score=winner_score, runner_up_score=runner_up_score,
         runner_up=runner_up, leaderboard=leaderboard, number=number
         )
-=======
-    return render_template("leaderboard.html", winner=winner, winner_score=winner_score, runner_up_score=runner_up_score, runner_up=runner_up, leaderboard=leaderboard, number=number)
->>>>>>> dc008c2f17a4d9e19c4b4a3d74b8e6f5673ca665
 
 # ..................................Run Web App
 
